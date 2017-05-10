fc2f34f8c554a1ee9eab9b6e04509f246ad3587b
Justin Gomez and Tyler Allen <tyler@>
2017-05-10 16:35:13 -0700

    add large single button that outputs value when clicked.

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+//BACK-END LOGIC
+
+//UI LOGIC
+$(document).ready(function() {
+  $("#top-Left").click(function(){
+    var topLeft = $(this).val();
+    alert(topLeft);
+    $("#top-Left").append("X GON GIVE IT TO YA");
+
+
+  });
+});
