f08d41c7fc340f5f48a9e7401af7dac4fde7173e
Justin Gomez <programjustin@gmail.com>
2017-05-10 20:22:05 -0700

    toggle input buttons everytime an input button is clicked on

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,47 @@
 //BACK-END LOGIC
+function User(name) {
+  this.name = name;
+}
+
+User.prototype.pass = function() {
+
+}
 
 //UI LOGIC
 $(document).ready(function() {
-  $("#top-Left").click(function(){
-    var topLeft = $(this).val();
-    alert(topLeft);
-    $("#top-Left").append("X GON GIVE IT TO YA");
 
+  $(".styleBox").click(function(){
+
+    var topLeft = $(this).val();
+    var topMiddle = $(this).val();
+    var topRight = $(this).val();
+    var middleLeft = $(this).val();
+    var middleMiddle = $(this).val();
+    var middleRight = $(this).val();
+    var bottomLeft = $(this).val();
+    var bottomMiddle = $(this).val();
+    var bottomRight = $(this).val();
+
+    $(".styleBox").append("X");
+    $(".styleBox").toggle();
+    $(".initiallyHidden").toggle();
+
+  });
+  $(".initiallyHidden").click(function(){
+
+    var topLeft = $(this).val();
+    var topMiddle = $(this).val();
+    var topRight = $(this).val();
+    var middleLeft = $(this).val();
+    var middleMiddle = $(this).val();
+    var middleRight = $(this).val();
+    var bottomLeft = $(this).val();
+    var bottomMiddle = $(this).val();
+    var bottomRight = $(this).val();
+
+    $(".initiallyHidden").append("O");
+    $(".styleBox").toggle();
+    $(".initiallyHidden").toggle();
 
   });
 });
