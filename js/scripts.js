91370ff9537b3b86463b7b9b52cf57e62b5b26be
justin gomez and tyler allen <justin@>
2017-05-16 14:09:56 -0700

    refactor to paste into other project

--- js/scripts.js
+++ js/scripts.js
@@ -17,43 +17,47 @@ Player.prototype.checkForWinner = function(length,index){
     $("button").hide();
     $(".container").fadeOut(4500);
     if (index === 1) {
-      $(".locustGif").fadeIn(5100);
+      $(".goodGuyImage").fadeIn(5100);
     }
     else if (index === 2) {
-      $(".cogGif").fadeIn(5100);
+      $(".badGuyImage").fadeIn(5100);
     }
   }
   else if (length === 9) {
     $(".container").fadeOut(4500);
-    $("#drawGif").fadeIn(5100);
+    $("#drawImage").fadeIn(5100);
+  }
+  else {
+    return "no winner";
   }
 }
 
 //UI LOGIC
 $(document).ready(function() {
 
-  // $(vs user button).click(function(){
-    var playerOne = new Player();
-    var cpuTicTacToe = new Player();
-    var boxesChecked = new Player();
+  var playerOne = new Player();
+  var cpuTicTacToe = new Player();
+  var boxesChecked = new Player();
 
-    var availableSquares = ["R1C1", "R1C2", "R1C3", "R2C1", "R2C2", "R2C3", "R3C1", "R3C2", "R3C3"];
-    var availableSquaresLocation = ["#R1C1", "#R1C2", "#R1C3", "#R2C1", "#R2C2", "#R2C3", "#R3C1", "#R3C2", "#R3C3"];
-    var index = 1;
+  var availableSquares = ["R1C1", "R1C2", "R1C3", "R2C1", "R2C2", "R2C3", "R3C1", "R3C2", "R3C3"];
+  var availableSquaresLocation = ["#R1C1", "#R1C2", "#R1C3", "#R2C1", "#R2C2", "#R2C3", "#R3C1", "#R3C2", "#R3C3"];
+  var index = 1;
 
-    //Player clicks square
-    $("button").on("click", function(){
-      $(this).parent().addClass('X');
-      $(this).hide();
-      var playerOnePosition = $(this).val();
-      playerOne.captured.push(playerOnePosition);
-      boxesChecked.captured.push(playerOnePosition);
-      availableSquares.splice(availableSquares.indexOf(playerOnePosition), 1);
-      availableSquares.splice(availableSquaresLocation.indexOf(playerOnePosition), 1);
-      playerOne.checkForWinner(boxesChecked.captured.length, index);
-      index = 2;
+  //Player clicks square
+  $("button").on("click", function(){
+    var playerOnePosition = $(this).val();
+    playerOne.captured.push(playerOnePosition);
+    boxesChecked.captured.push(playerOnePosition);
+    availableSquares.splice(availableSquares.indexOf(playerOnePosition), 1);
+    availableSquaresLocation.splice(availableSquaresLocation.indexOf(playerOnePosition), 1);
+    var isThereAWinner = playerOne.checkForWinner(boxesChecked.captured.length, index);
+    $(this).parent().addClass('X');
+    $(this).hide();
+    index = 2;
 
+    if (isThereAWinner === "no winner") {
       for( var i = 1; i < 2; i += 1) {
+        console.log(availableSquares);
         //CPU selects random number
         var cpuRandomNumber = Math.floor(Math.random()*availableSquares.length);
         var cpuPosition = availableSquares.splice(cpuRandomNumber, 1);
@@ -66,6 +70,8 @@ $(document).ready(function() {
         boxesChecked.captured.push(cpuPosition);
         cpuTicTacToe.checkForWinner(boxesChecked.captured.length, index);
         index = 1;
+        console.log(availableSquares);
       };
-    });
+    };
+  });
 });
